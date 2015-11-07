49d3056115a5a7f1de8ff2dd6ae82bf038427042
Alex de Kruijff <akruijff@dds.nl>
2015-11-07 14:32:33 +0100

    Intiail commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Alex de Kruijff <alex.de.kruijff@MazarineBlue.org>
+ *
+ * Created on November 7, 2015, 2:32 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
